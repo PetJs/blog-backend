d142b336a4bb132ef9a781e42c1905f837c7c219
PetJs <peterfagoroye76@gmail.com>
2025-09-06 15:00:14 +0100

    Add user models

---
+++ internal/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey"`
+	Username  string    `gorm:"size:100;unique;not null"`
+	Email     string    `gorm:"size:255;unique;not null"`
+	Password  string    `gorm:"size:255;not null"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+}

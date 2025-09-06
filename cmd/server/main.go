4cfd534e906cd49bcac5c2f123492a1ef5418eb6
PetJs <peterfagoroye76@gmail.com>
2025-09-06 15:01:49 +0100

    Creates server entry

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"github.com/PetJs/blog-backend/pkg/config"
+	"github.com/PetJs/blog-backend/pkg/database"
+	"github.com/PetJs/blog-backend/internal/repository"
+	"github.com/PetJs/blog-backend/internal/services"
+	"github.com/PetJs/blog-backend/internal/api"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main(){
-	fmt.Println("Hello, World!")
+	cfg := config.LoadConfig()
+	
+	
+	db := database.ConnectDB(cfg.DB_URL)
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+
+	repo := repository.NewPostRepository(sqlDB)
+	service := services.NewPostService(repo)
+
+
+	router := gin.Default()
+	api.RegisterPostRoutes(router, service)
+
+	log.Println("Server running on port " + cfg.Port)
+
+	router.Run(":" + cfg.Port)
 }

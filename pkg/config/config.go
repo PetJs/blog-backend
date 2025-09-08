c17915fb2474a80df48c52d85d223fecf7a25991
PetJs <peterfagoroye76@gmail.com>
2025-09-08 15:43:14 +0100

    Modify config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,14 @@
 package config
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 type Config struct {
 	Port string
 	DB_URL string
+	JWT_SECRET_KEY string
 }
 
 
@@ -12,11 +16,13 @@ func LoadConfig() Config {
 	return Config{
 		Port: getEnv("PORT", "8080"),
 		DB_URL: getEnv("DB_URL", "root:Morowa@tcp(127.0.0.1:3306)/blogdb?charset=utf8mb4&parseTime=True&loc=Local"),
+		JWT_SECRET_KEY: getEnv("JWT_SECRET_KEY", "SHABALALA"),
 	}
 }
 
 func getEnv(key, fallback string) string{
 	if value, ok := os.LookupEnv(key); ok {
+		fmt.Println("The env variable is: ", value)
 		return value
 	}
 	return fallback

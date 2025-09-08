693a573276cc41962d8bbcc138ca84dfa364cba7
PetJs <peterfagoroye76@gmail.com>
2025-09-08 15:43:28 +0100

    Adds user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `gorm:"primaryKey"`
-	Username  string    `gorm:"size:100;unique;not null"`
-	Email     string    `gorm:"size:255;unique;not null"`
+	ID        uint      `gorm:"primaryKey" json:"user_id"`
+	Username  string    `gorm:"size:100;unique;not null" json:"username"`
+	Email     string    `gorm:"size:255;unique;not null" json:"email"`
 	Password  string    `gorm:"size:255;not null"`
-	CreatedAt time.Time `gorm:"autoCreateTime"`
+	CreatedAt *time.Time `json:"created_at"`
 }

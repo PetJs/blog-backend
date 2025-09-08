a819fbd6ab7fa21e529e2ad260021355973209cb
PetJs <peterfagoroye76@gmail.com>
2025-09-08 15:42:42 +0100

    Adds user service

--- internal/repository/user_repo.go
+++ internal/repository/user_repo.go
@@ -1,1 +1,26 @@
 package repository
+
+import (
+	"gorm.io/gorm"
+	"github.com/PetJs/blog-backend/internal/models"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{DB: db}
+}
+
+func (r *UserRepository) CreateUser(user *models.User) error {
+	return r.DB.Create(user).Error
+}
+
+func (r *UserRepository) GetUserByEmail(email string) (*models.User, error){
+	var user models.User
+	if err := r.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
--- internal/services/user_service.go
+++ internal/services/user_service.go
@@ -1,1 +1,44 @@
 package services
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/PetJs/blog-backend/internal/models"
+	"github.com/PetJs/blog-backend/internal/repository"
+)
+
+type UserService struct {
+	Repo *repository.UserRepository
+}
+
+func NewUserService(repo *repository.UserRepository) *UserService {
+	return &UserService{Repo: repo}
+}
+
+func (s *UserService) RegisterUser(user *models.User) error {
+	//Hash password before saving
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(hashedPassword)
+	return s.Repo.CreateUser(user)
+}
+
+func (s *UserService) LoginUser(email, password string) (*models.User, error) {
+	user, err := s.Repo.GetUserByEmail(email)
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	//Compare hashed password with the provided password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("invalid email or password")
+	}
+
+	return user, nil
+}

158084ce456fd06cfc4b227084ab18e0982598e2
PetJs <peterfagoroye76@gmail.com>
2025-09-08 15:43:00 +0100

    Adds Auth middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,1 +1,37 @@
 package middleware
+
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/PetJs/blog-backend/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header(token) is required"})
+			return
+		}
+
+		// Expected Format: "Bearer <token>"
+		tokenParts := strings.Split(authHeader, " ")
+		if len(tokenParts) != 2 || tokenParts[0] != "Bearer" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		user_id, err := utils.ValidateToken(tokenParts[1])
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		//save user_id into context
+		c.Set("user_id", user_id)
+		c.Next()
+	}
+}

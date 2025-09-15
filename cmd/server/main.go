b884d22e1f6486025117b62d3db7256e2f488317
PetJs <peterfagoroye76@gmail.com>
2025-09-15 10:31:28 +0100

    fix: Fixes CORS issue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"github.com/PetJs/blog-backend/pkg/config"
-	"github.com/PetJs/blog-backend/pkg/database"
+	"log"
+	"time"
+
+	"github.com/PetJs/blog-backend/internal/api"
 	"github.com/PetJs/blog-backend/internal/repository"
 	"github.com/PetJs/blog-backend/internal/services"
-	"github.com/PetJs/blog-backend/internal/api"
-	"log"
+	"github.com/PetJs/blog-backend/pkg/config"
+	"github.com/PetJs/blog-backend/pkg/database"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
@@ -27,6 +30,14 @@ func main(){
 
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  	  []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	api.RegisterPostRoutes(router, service)
 	api.RegisterUserRoutes(router, userService)
 

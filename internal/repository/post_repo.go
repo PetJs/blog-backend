a963654ec0afdc815df3f41baccf62eca70ac032
Oluwamorowa <peterfagoroye76@gmail.com>
2026-04-18 16:07:07 +0100

    refactor: streamline PostRepository methods and improve code readability

--- internal/repository/post_repo.go
+++ internal/repository/post_repo.go
@@ -10,53 +10,48 @@ type PostRepository struct {
 	DB *gorm.DB
 }
 
-func NewPostRepository(db *gorm.DB) *PostRepository{
+func NewPostRepository(db *gorm.DB) *PostRepository {
 	return &PostRepository{DB: db}
 }
 
-func (r *PostRepository) GetAllPosts() ([]models.Post, error){
-
+func (r *PostRepository) GetAllPosts() ([]models.Post, error) {
 	var posts []models.Post
-	if err := r.DB.Preload("User").Order("created_at DESC").Find(&posts).Error; err != nil {
-        return nil, err
-    }
+	if err := r.DB.Order("created_at DESC").Find(&posts).Error; err != nil {
+		return nil, err
+	}
 	return posts, nil
+}
+
+func (r *PostRepository) GetPostByID(id string) (*models.Post, error) {
+	var post models.Post
+	if err := r.DB.First(&post, id).Error; err != nil {
+		return nil, err
+	}
+	return &post, nil
 }
 
 func (r *PostRepository) CreatePost(post *models.Post) (*models.Post, error) {
 	if err := r.DB.Create(post).Error; err != nil {
-        return nil, err
-    }
-
-    // Reload post with user info
-    if err := r.DB.Preload("User").First(post, post.ID).Error; err != nil {
-        return nil, err
-    }
-
-    return post, nil
-}
-
-func (r *PostRepository) DeletePost(postID string, userID uint) error {
-	var post models.Post
-
-	//find thepost by id
-	if err := r.DB.First(&post, postID).Error; err != nil {
-		return err
-	}
-
-	//Check if the post belongs to the user
-	if post.UserID != userID {
-		return gorm.ErrRecordNotFound
-	}
-
-	return r.DB.Delete(&post).Error
-}
-
-func (r *PostRepository) GetPostByUser(userID uint) ([]models.Post, error){
-	var posts []models.Post
-
-	if err := r.DB.Preload("User").Where("user_id = ?", userID).Order("created_at DESC").Find(&posts).Error; err != nil {
 		return nil, err
 	}
-	return posts, nil
+	return post, nil
+}
+
+func (r *PostRepository) UpdatePost(id string, updates map[string]interface{}) (*models.Post, error) {
+	var post models.Post
+	if err := r.DB.First(&post, id).Error; err != nil {
+		return nil, err
+	}
+	if err := r.DB.Model(&post).Updates(updates).Error; err != nil {
+		return nil, err
+	}
+	return &post, nil
+}
+
+func (r *PostRepository) DeletePost(id string) error {
+	var post models.Post
+	if err := r.DB.First(&post, id).Error; err != nil {
+		return err
+	}
+	return r.DB.Delete(&post).Error
 }

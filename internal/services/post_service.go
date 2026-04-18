c0beb86cd937e16901e3c420869d483f3ed2aa75
Oluwamorowa <peterfagoroye76@gmail.com>
2026-04-18 16:06:23 +0100

    refactor: clean up PostService methods and improve formatting

--- internal/services/post_service.go
+++ internal/services/post_service.go
@@ -1,38 +1,34 @@
 package services
 
 import (
-
 	"github.com/PetJs/blog-backend/internal/models"
 	"github.com/PetJs/blog-backend/internal/repository"
 )
-
 
 type PostService struct {
 	Repo *repository.PostRepository
 }
 
-func NewPostService(repo *repository.PostRepository) *PostService{
+func NewPostService(repo *repository.PostRepository) *PostService {
 	return &PostService{Repo: repo}
 }
 
-func (s *PostService) GetPosts() ([]models.Post, error){
+func (s *PostService) GetPosts() ([]models.Post, error) {
 	return s.Repo.GetAllPosts()
 }
 
-func (s *PostService) CreatePost(post models.Post) (models.Post, error) {
-	savedPost, err := s.Repo.CreatePost(&post)
-    if err != nil {
-        return models.Post{}, err
-    }
-    return *savedPost, nil
+func (s *PostService) GetPost(id string) (*models.Post, error) {
+	return s.Repo.GetPostByID(id)
 }
 
-func (s *PostService) DeletePost(postID string, userID uint) error {
-	return s.Repo.DeletePost(postID, userID)
+func (s *PostService) CreatePost(post models.Post) (*models.Post, error) {
+	return s.Repo.CreatePost(&post)
 }
 
-
-func (s *PostService) GetUserPosts(userID uint) ([]models.Post, error) {
-	return s.Repo.GetPostByUser(userID)
+func (s *PostService) UpdatePost(id string, updates map[string]interface{}) (*models.Post, error) {
+	return s.Repo.UpdatePost(id, updates)
 }
 
+func (s *PostService) DeletePost(id string) error {
+	return s.Repo.DeletePost(id)
+}
